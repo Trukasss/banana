9850a09e6794f736bdab6eb7b50416fc6ba7b0b5
Lukas Sabaliauskas <lukas_sabaliauskas@hotmail.com>
2022-04-12 13:18:53 +0200

    lukas wip
    Signed-off-by: Lukas Sabaliauskas <lukas_sabaliauskas@hotmail.com>

---
+++ Assets/WIP_Lukas/script_game_logic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_game_logic : MonoBehaviour
+{
+    //VARIABLES
+    public float temps, score;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
