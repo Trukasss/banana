df6b5163419c119295c034c3f464ec411be1bf26
Soren <soren.pichot@orange.fr>
2022-04-15 11:53:04 +0200

    Ajout VR 2
    Signed-off-by: Oror

---
+++ Assets/WIP_Oror/CameraFreeze.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFreeze : MonoBehaviour
+{
+    // Update is called once per frame
+    void Awake()
+    {
+        UnityEngine.XR.XRDevice.DisableAutoXRCameraTracking(Camera.main, true);
+    }
+
+}
