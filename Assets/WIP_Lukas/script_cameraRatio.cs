7100a4e5f4b9a09b40dc1dfe2429c813ef957913
Lukas Sabaliauskas <lukas_sabaliauskas@hotmail.com>
2022-04-15 15:17:17 +0200

    camera stretch
    Signed-off-by: Lukas Sabaliauskas <lukas_sabaliauskas@hotmail.com>

---
+++ Assets/WIP_Lukas/script_cameraRatio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_cameraRatio : MonoBehaviour
+{
+    Camera cam;
+    public float height = 1f;
+    public float width = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //stretch view//
+        cam.ResetProjectionMatrix();
+        var m = cam.projectionMatrix;
+
+        m.m11 *= height;
+        m.m00 *= width;
+        cam.projectionMatrix = m;
+    }
+}
